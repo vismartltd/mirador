82d7a1ccaa342cb36df1f612cdd9665209f5a601
stanislavkostadinov <stanislav.kostadinov@sirma.bg>
2016-01-08 16:48:43 +0200

    update of transformation of ellipses

--- js/src/annotations/osd-svg-ellipse.js
+++ js/src/annotations/osd-svg-ellipse.js
@@ -3,8 +3,7 @@
     jQuery.extend(this, {
       name: 'Ellipse',
       logoClass: 'fa-circle-o',
-      idPrefix: 'circle_',
-      ellipseSegments: []
+      idPrefix: 'circle_'
     }, options);
 
     this.init();
@@ -15,25 +14,38 @@
 
     createShape: function(initialPoint, overlay) {
       overlay.mode = 'create';
+      var segments = [];
+      // point indexes
+      //       1      
+      //   0   ─   2  
+      //     /   \    
+      //  7 |     | 3 
+      //     \   /    
+      //   6   ─   4  
+      //       5      
+      segments.push(new Point(initialPoint.x - 2, initialPoint.y - 2));
+      segments.push(new Point(initialPoint.x - 1, initialPoint.y - Math.sqrt(2) - 1));
+      segments.push(new Point(initialPoint.x, initialPoint.y - 2));
+      segments.push(new Point(initialPoint.x + Math.sqrt(2) - 1, initialPoint.y - 1));
+      segments.push(new Point(initialPoint.x, initialPoint.y));
+      segments.push(new Point(initialPoint.x - 1, initialPoint.y + Math.sqrt(2) - 1));
+      segments.push(new Point(initialPoint.x - 2, initialPoint.y));
+      segments.push(new Point(initialPoint.x - Math.sqrt(2) - 1, initialPoint.y - 1));
       var _this = this;
-      var shape = new Path.Ellipse({
-        center: [initialPoint.x - 1, initialPoint.y - 1],
-        radius: [1, 1]
+      var shape = new Path({
+        segments: segments,
+        fullySelected: true,
+        name: _this.idPrefix + (project.getItems({
+          name: /_/
+        }).length + 1)
       });
       shape.strokeColor = overlay.strokeColor;
       shape.fillColor = overlay.fillColor;
       shape.fillColor.alpha = overlay.fillColorAlpha;
-      shape.name = _this.idPrefix + (project.getItems({
-        name: /_/
-      }).length + 1);
-      for (var j = 0; j < shape.segments.length; j++) {
-        if (shape.segments[j].point.x == initialPoint.x) {
-          _this.ellipseSegments[0] = shape.segments[j];
-        }
-        if (shape.segments[j].point.y == initialPoint.y) {
-          _this.ellipseSegments[1] = shape.segments[j];
-        }
-      }
+      shape.closed = true;
+      shape.smooth();
+      shape.data.rotation = 0;
+      overlay.segment = shape.segments[4];
       return shape;
     },
 
@@ -47,45 +59,34 @@
           overlay.path.segments[i].point.x += event.delta.x;
           overlay.path.segments[i].point.y += event.delta.y;
         }
-      } else if (overlay.mode == 'deform') {
-        var centerX = 0;
-        var centerY = 0;
-        var currIndex = -1;
-        for (var j = 0; j < overlay.path.segments.length; j++) {
-          centerX += overlay.path.segments[j].point.x;
-          centerY += overlay.path.segments[j].point.y;
-          if (overlay.path.segments[j].point.x == overlay.segment.point.x && overlay.path.segments[j].point.y == overlay.segment.point.y) {
-            currIndex = j;
-          }
-        }
-        centerX /= overlay.path.segments.length;
-        centerY /= overlay.path.segments.length;
-        var oldPoint = new Point(overlay.segment.point.x - centerX, overlay.segment.point.y - centerY);
-        var newPoint = new Point(overlay.segment.point.x - centerX + event.delta.x, overlay.segment.point.y - centerY + event.delta.y);
-        var scale = Math.sqrt(newPoint.x * newPoint.x + newPoint.y * newPoint.y) / Math.sqrt(oldPoint.x * oldPoint.x + oldPoint.y * oldPoint.y);
-        var rotation = Math.atan2(newPoint.y, newPoint.x) - Math.atan2(oldPoint.y, oldPoint.x);
-        rotation = rotation * (180 / Math.PI);
-        overlay.path.scale(scale);
-        overlay.path.rotate(rotation, new Point(centerX, centerY));
-      } else if (overlay.path) {
-        var cX = 0;
-        var cY = 0;
+      } else if (overlay.mode == 'create' || overlay.mode == 'deform') {
+        var idx = -1;
         for (var l = 0; l < overlay.path.segments.length; l++) {
-          cX += overlay.path.segments[l].point.x;
-          cY += overlay.path.segments[l].point.y;
-        }
-        cX /= overlay.path.segments.length;
-        cY /= overlay.path.segments.length;
-        var scaleX = 1;
-        for (var m = 0; m < overlay.path.segments.length; m++) {
-          if (overlay.path.segments[m].point.x == this.ellipseSegments[0].point.x) {
-            scaleX = (overlay.path.segments[m].point.x - cX + event.delta.x) / (overlay.path.segments[m].point.x - cX);
-          }
-          if (overlay.path.segments[m].point.y == this.ellipseSegments[1].point.y) {
-            scaleY = (overlay.path.segments[m].point.y - cY + event.delta.y) / (overlay.path.segments[m].point.y - cY);
+          if (overlay.path.segments[l] == overlay.segment) {
+            idx = l;
+            break;
           }
         }
-        overlay.path.scale([scaleX, scaleY]);
+        if (overlay.mode == 'deform' && idx % 2 == 1) {
+          var oldPoint = new Point(overlay.segment.point.x - overlay.path.position.x, overlay.segment.point.y - overlay.path.position.y);
+          var newPoint = new Point(overlay.segment.point.x - overlay.path.position.x + event.delta.x, overlay.segment.point.y - overlay.path.position.y + event.delta.y);
+          var scale = Math.sqrt(newPoint.x * newPoint.x + newPoint.y * newPoint.y) / Math.sqrt(oldPoint.x * oldPoint.x + oldPoint.y * oldPoint.y);
+          var rotation = Math.atan2(newPoint.y, newPoint.x) - Math.atan2(oldPoint.y, oldPoint.x);
+          rotation = rotation * (180 / Math.PI);
+          overlay.path.scale(scale);
+          overlay.path.rotate(rotation, overlay.path.position);
+          overlay.path.data.rotation += rotation;
+        } else {
+          var oldRotPoint = new Point(overlay.segment.point.x - overlay.path.position.x, overlay.segment.point.y - overlay.path.position.y);
+          var newRotPoint = new Point(overlay.segment.point.x - overlay.path.position.x + event.delta.x, overlay.segment.point.y - overlay.path.position.y + event.delta.y);
+          var rot = overlay.path.data.rotation;
+          oldRotPoint = oldRotPoint.rotate(-rot);
+          newRotPoint = newRotPoint.rotate(-rot);
+          var rotScale = new Point(newRotPoint.x / oldRotPoint.x, newRotPoint.y / oldRotPoint.y);
+          overlay.path.rotate(-rot, overlay.path.position);
+          overlay.path.scale(rotScale);
+          overlay.path.rotate(rot, overlay.path.position);
+        }
       }
     },
 
@@ -96,7 +97,7 @@
     onMouseDown: function(event, overlay) {
       var hitResult = project.hitTest(event.point, overlay.hitOptions);
       if (hitResult && hitResult.item._name.toString().indexOf(this.idPrefix) != -1) {
-        if (overlay.mode != 'deform' && overlay.mode != 'translate') {
+        if (overlay.mode != 'deform' && overlay.mode != 'translate' && overlay.mode != 'create') {
           if (hitResult.type == 'segment') {
             overlay.mode = 'deform';
             overlay.segment = null;
