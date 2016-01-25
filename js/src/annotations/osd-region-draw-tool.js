92ac538e6348d42ac4c542d9cbbf8ce39c615399
stanislavkostadinov <stanislav.kostadinov@sirma.bg>
2016-01-25 15:04:04 +0200

    * edit/delete comments

--- js/src/annotations/osd-region-draw-tool.js
+++ js/src/annotations/osd-region-draw-tool.js
@@ -127,6 +127,17 @@
           fixed: true,
           delay: 300,
           event: false
+        },
+        events: {
+          visible: function(event, api) {
+            _this.removeAnnotationEvents(event, api);
+            _this.annotationEvents(event, api);
+          },
+          move: function(event, api) {
+            _this.removeAnnotationEvents(event, api);
+            _this.annotationEvents(event, api);
+            _this.annotationSaveEvent(event, api);
+          }
         }
       });
       var api = jQuery(this.osdViewer.element).qtip('api');
@@ -230,6 +241,155 @@
 
       jQuery.subscribe('enableTooltips.' + _this.parent.windowId, function() {
         _this.inEditOrCreateMode = false;
+      });
+    },
+
+    getAnnoFromRegion: function(regionId) {
+      return this.list.filter(function(annotation) {
+        return annotation['@id'] === regionId;
+      });
+    },
+
+    freezeQtip: function(api, oaAnno, annoTooltip) {
+      this.inEditOrCreateMode = true;
+      api.set({
+        'content.text': annoTooltip.getEditor(oaAnno),
+        'hide.event': false
+      });
+      jQuery.publish('annotationEditorAvailable.' + this.parent.windowId);
+      //add rich text editor
+      tinymce.init({
+        selector: 'form.annotation-tooltip textarea',
+        plugins: "image link media",
+        menubar: false,
+        statusbar: false,
+        toolbar_items_size: 'small',
+        toolbar: "bold italic | bullist numlist | link image media | removeformat",
+        setup: function(editor) {
+          editor.on('init', function(args) {
+            tinymce.execCommand('mceFocus', false, args.target.id);
+          });
+        }
+      });
+      jQuery(api.elements.tooltip).removeClass("qtip-viewer");
+    },
+
+    unFreezeQtip: function(api, oaAnno, annoTooltip) {
+      this.inEditOrCreateMode = false;
+      api.set({
+        'content.text': annoTooltip.getViewer([oaAnno]),
+        'hide.event': 'mouseleave'
+      }).hide();
+      jQuery(api.elements.tooltip).addClass("qtip-viewer");
+    },
+
+    removeAnnotationEvents: function(tooltipevent, api) {
+      var _this = this;
+      var editorSelector = '#annotation-editor-' + _this.parent.windowId;
+      var viewerSelector = '#annotation-viewer-' + _this.parent.windowId;
+      jQuery(viewerSelector + ' a.delete').off("click");
+      jQuery(viewerSelector + ' a.edit').off("click");
+      jQuery(editorSelector + ' a.save').off("click");
+      jQuery(editorSelector + ' a.cancel').off("click");
+    },
+
+    annotationEvents: function(tooltipevent, api) {
+      var _this = this;
+      var annoTooltip = new $.AnnotationTooltip({"windowId": _this.parent.windowId});
+      var selector = '#annotation-viewer-' + _this.parent.windowId;
+      jQuery(selector + ' a.delete').on("click", function(event) {
+        event.preventDefault();
+        if (!window.confirm("Do you want to delete this annotation?")) {
+          return false;
+        }
+        var display = jQuery(this).parents('.annotation-display');
+        var id = display.attr('data-anno-id');
+        jQuery.publish('annotationDeleted.' + _this.parent.windowId, [id]);
+        api.hide();
+        display.remove();
+      });
+
+      jQuery(selector + ' a.edit').on("click", function(event) {
+        event.preventDefault();
+        var display = jQuery(this).parents('.annotation-display');
+        var id = display.attr('data-anno-id');
+        var oaAnno = _this.getAnnoFromRegion(id)[0];
+        _this.freezeQtip(api, oaAnno, annoTooltip);
+        _this.annotationSaveEvent(event, api);
+      });
+    },
+
+    annotationSaveEvent: function(event, api) {
+      var _this = this;
+      var annoTooltip = new $.AnnotationTooltip({"windowId": _this.parent.windowId});
+      var selector = '#annotation-editor-' + _this.parent.windowId;
+
+      jQuery(selector).on("submit", function(event) {
+        event.preventDefault();
+        jQuery(selector + ' a.save').click();
+      });
+
+      jQuery(selector + ' a.save').on("click", function(event) {
+        event.preventDefault();
+        var display = jQuery(this).parents('.annotation-editor');
+        var id = display.attr('data-anno-id');
+        var oaAnno = _this.getAnnoFromRegion(id)[0];
+
+        var tagText = jQuery(this).parents('.annotation-editor').find('.tags-editor').val(),
+          resourceText = tinymce.activeEditor.getContent(),
+          tags = [];
+        tagText = $.trimString(tagText);
+        if (tagText) {
+          tags = tagText.split(/\s+/);
+        }
+
+        var bounds = _this.svgOverlay.viewer.viewport.getBounds(true);
+        var scope = _this.svgOverlay.viewer.viewport.viewportToImageRectangle(bounds);
+
+        if (!oaAnno.on.scope) {
+          oaAnno.on.scope = {
+            "@context": "http://www.harvard.edu/catch/oa.json",
+            "@type": "catch:Viewport"
+          };
+        }
+        oaAnno.on.scope.value = "xywh=" + Math.round(scope.x) + "," + Math.round(scope.y) + "," + Math.round(scope.width) + "," + Math.round(scope.height); //osd bounds
+        // oaAnno.on.selector.value remains the same
+
+        var motivation = [],
+          resource = [];
+
+        //remove all tag-related content in annotation
+        oaAnno.motivation = jQuery.grep(oaAnno.motivation, function(value) {
+          return value !== "oa:tagging";
+        });
+        oaAnno.resource = jQuery.grep(oaAnno.resource, function(value) {
+          return value["@type"] !== "oa:Tag";
+        });
+        //re-add tagging if we have them
+        if (tags.length > 0) {
+          oaAnno.motivation.push("oa:tagging");
+          jQuery.each(tags, function(index, value) {
+            oaAnno.resource.push({
+              "@type": "oa:Tag",
+              "chars": value
+            });
+          });
+        }
+        jQuery.each(oaAnno.resource, function(index, value) {
+          if (value["@type"] === "dctypes:Text") {
+            value.chars = resourceText;
+          }
+        });
+        //save to endpoint
+        jQuery.publish('annotationUpdated.' + _this.parent.windowId, [oaAnno]);
+      });
+
+      jQuery(selector + ' a.cancel').on("click", function(event) {
+        event.preventDefault();
+        var display = jQuery(this).parents('.annotation-editor');
+        var id = display.attr('data-anno-id');
+        var oaAnno = _this.getAnnoFromRegion(id)[0];
+        _this.unFreezeQtip(api, oaAnno, annoTooltip);
       });
     }
   };
